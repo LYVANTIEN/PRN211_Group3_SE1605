404079d0180655613a94d7c6f284335d5c6d667c
MinhHo204 <minhhonhat.work@gmail.com>
2023-10-07 12:32:54 +0700

    Update Add controller
    Update Add controller

---
+++ CinemaWed/Controllers/TheaterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaWed.Controllers
+{
+    public class TheaterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
