91942b2be4da769afa72db97a3686902df2c9ba7
Tienvan2112 <55881057+LYVANTIEN@users.noreply.github.com>
2023-10-14 08:29:25 +0700

    new update

--- CinemaWed/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CinemaWed.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
