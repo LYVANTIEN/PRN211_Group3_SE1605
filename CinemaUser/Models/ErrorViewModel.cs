af26386922b9f4954290484b75ef3cd4a061721b
Tienvan2112 <55881057+LYVANTIEN@users.noreply.github.com>
2023-10-14 08:37:19 +0700

    new

--- CinemaUser/Models/ErrorViewModel.cs
+++ CinemaUser/Models/ErrorViewModel.cs
@@ -1,7 +1,7 @@
 namespace CinemaUser.Models
 {
     public class ErrorViewModel
-    {
+    {//aa
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
